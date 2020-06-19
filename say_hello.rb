af79960cd8ddcf3e1fc4c0f572d0e5602e91ecad
Liza Tillman <lizaetillman@gmail.com>
2020-06-19 00:12:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,15 @@
+# says programer's name
 def say_hello(name)
   puts "Hello #{name}!"
+end
+
+# assigning default value
+def say_hello(name = "Ruby Programmer!")
+  puts "Hello #{name}"
 end
 
 say_hello("Gabriela")
 # Hello Gabriela!
 
 say_hello(name)
-# Hello Ruby Programmer! 
+# Hello Ruby Programmer!
